0f6c3be140bd44b849431d462b2a592b3fb9b02f
Philipp Kraus <blinddog@ebkraus.de>
2023-05-11 22:22:18 +0200

    First draft for user creation

---
+++ functions/src/config.ts
@@ -0,0 +1,2 @@
+export const STRAVA_BASE_PATH = "https://www.strava.com/api/v3";
+export const ATHLETE_PATH = "athlete";
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,12 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 import * as express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { devSecret, prodSecret } from "./credentials";
+import axios, { AxiosRequestConfig } from "axios";
+import { ATHLETE_PATH, STRAVA_BASE_PATH } from "./config";
+
+admin.initializeApp();
 
 const app = express();
 const stravaAuthProxy = (secret: string) =>
@@ -17,3 +22,23 @@ const stravaAuthProxy = (secret: string) =>
 app.use("/oauth", stravaAuthProxy(prodSecret));
 app.use("/dev/oauth", stravaAuthProxy(devSecret));
 exports.stravaproxy = functions.https.onRequest(app);
+exports.tokenExchange = functions.https.onRequest(async (req, res) => {
+  // Call strava /athlete
+  const config: AxiosRequestConfig = {
+    baseURL: STRAVA_BASE_PATH,
+    headers: { Authorization: `${req.get("Authorization")}` },
+    method: "get",
+  };
+
+  const response = await axios.get(`/${ATHLETE_PATH}`, config);
+
+  console.log(response.status, response.data);
+
+  // Create user if not yet exists
+  await admin.auth().createUser({
+    email: "blinddog@hotmail.de",
+    photoURL: response.data.profile,
+  });
+
+  res.status(200).send();
+});
